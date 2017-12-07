e20a26a79f7928db34d81e1a7a95e78934284dd8
raphisky <raphael.kammoun@gmail.com>
2017-12-07 10:21:39 +0100

    update lists

--- trends.js
+++ trends.js
@@ -19,13 +19,13 @@ var spin_countVariations = function (spun) {
   return eval(spun);
  }
 
-var subjects = "{Booba|Elon Musk|Taylor Swift|Kanye|Macron|Trump|Deschamps|Cyril Hanouna|Céline Dion|Theresa May|Une intelligence artificielle|Konbini|La classe politique|Une illustre inconnue|Rihanna|Un truc jaune qui attend|Un panda roux|Mbappé|Un millennial|Une dominatrice}";
+var subjects = "{Booba|Elon Musk|Cardi B|Kanye|Macron|Trump|Deschamps|Cyril Hanouna|Céline Dion|Theresa May|Une intelligence artificielle|Konbini|La classe politique|Une illustre inconnue|Rihanna|Un truc jaune qui attend|Un panda roux|Mbappé|Un millennial|Une dominatrice}";
 
 var verbs = "{plaquera tout pour|fera barrage à|fera tout pour|fera tout contre|abandonnera|commencera|sortira|soutiendra|s’illustrera par|investira dans|bannira|s’attaquera à|engagera|financera}";
 
 var complements = "{un pure player|l’ONU|la paix dans le monde|sa mixtape de 2018|un porc vietnamien|du pâté de campagne|sa gym suédoise|un joueur du Bayern|une course en sac|sa startup|une portée de chatons|une calvitie rampante|un restaurant arménien|le cinéma flamand|Jul|la blockchain|un abribus|l’huile de palme|une soirée electroswing|un jeu de mölki}";
 
-var adverbs = "{et cassera internet.|si Dieu le veut.|pourquoi pas, après tout ?|sans état d’âme.|toujours debout.|sans autre forme de procès.|le tout, sans les mains.|contre l’avis de sa mère.|et ce, chaque mois.|jusqu’à la fin des temps.|si on ne fait rien pour l’en empêcher.|sans qu’on puisse y faire quoi que ce soit.|sans gluten.|contre toute attente.|tant sur le plan tactique que sur le plan technique.|au grand dam de la génération Z.|faisant fi des convenances.|réalisant son rêve d’enfant.|en dépit de la conjoncture.|si le temps le permet.|par amour du beau jeu}";
+var adverbs = "{et cassera internet.|si Dieu le veut.|sans état d’âme.|toujours debout.|sans autre forme de procès.|le tout, sans les mains.|contre l’avis de sa mère.|et ce, chaque mois.|jusqu’à la fin des temps.|si on ne fait rien pour l’en empêcher.|sans qu’on puisse y faire quoi que ce soit.|sans gluten.|contre toute attente.|tant sur le plan tactique que sur le plan technique.|au grand dam de la génération Z.|faisant fi des convenances.|réalisant son rêve d’enfant.|en dépit de la conjoncture.|si le temps le permet.|par amour du beau jeu|selon un site complotiste}";
 
   document.getElementById('generate').onclick = function() {
       var subj = document.getElementById('subject');
