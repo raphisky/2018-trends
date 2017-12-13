5565533f5fadf66d3cb2fa8ef3270e22910b82b3
raphisky <raphael.kammoun@gmail.com>
2017-12-13 16:11:58 +0100

    adds english changes style updates lists

--- trends.js
+++ trends.js
@@ -19,6 +19,8 @@ var spin_countVariations = function (spun) {
   return eval(spun);
  }
 
+// FRENCH
+
 var subjects = "{Niska|Elon Musk|Cardi B|Macron|Trump|Deschamps|Theresa May|Une IA|Un•e ministre|Rihanna|Mbappé|Un millennial}";
 
 var verbs = "{plaque tout pour|émet des doutes sur|investit dans|disrupte|soutient|révolutionne|dénonce|fait la promo pour|acclame|se passionne pour|découvre|soûle tout le monde avec}";
@@ -27,8 +29,20 @@ var complements = "{la K-Pop|la paix dans le monde|le crossfit|la bière sans al
 
 var adverbs = "{et casse internet.|parce que #goals.|en VR.|par amour du beau jeu.|selon un site complotiste.|contre l’avis de sa mère.|jusqu’à la fin des temps.|avec le sourire.|contre toute attente.|faisant fi des convenances.|réalisant son rêve d’enfant.|si le temps le permet.}";
 
+// ENGLISH
+
+var subjects_en = "{Donald Glover|Elon Musk|Cardi B|Macron|Trump|Floyd Mayweather|Angela Merkel|An AI|Serena Williams|Kendrick Lamar|Selena Gomez|A millennial}";
+
+var verbs_en = "{funds|makes a diss track against|opposes|hacks|only snaps about|speaks against|discovers|gives up on|promotes|disrupts|won’t shut up about|keeps bitching about}";
+
+var complements_en = "{world peace|Bitcoin|Emo-rap|acroyoga|several baby otters|avocado lattes|palm oil|global warming|root beer|space exploration|millennial pink|man buns}";
+
+var adverbs_en = "{and breaks the internet|going the extra mile|as one does.|leaving nothing to chance.|despite Mother’s advice.|to the surprise of no one.|against all odds.|ignoring all criticism.|realising a childhood dream.|experts forecast.|in VR though.|because #goals.}";
+
 
   document.getElementById('generate').onclick = function() {
       var subj = document.getElementById('subject');
+      var subj_en = document.getElementById('subject_en');
       subj.innerText = spin(subjects) + " " + spin(verbs) + " " + spin(complements) + ", " + spin(adverbs);
+      subj_en.innerText = spin(subjects_en) + " " + spin(verbs_en) + " " + spin(complements_en) + ", " + spin(adverbs_en);
   }
